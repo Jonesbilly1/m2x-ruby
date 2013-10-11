d59a5363c23fdb66407724f3c7b0f8e18cfff93d
Leandro López (inkel) <inkel.ar@gmail.com>
2013-10-10 21:20:23 -0300

    Add simple M2X object and HTTP client
    This object will instantiate a M2X::Client when required, which is in
    charge of sending all the requests to AT&T M2X API servers.

---
+++ lib/m2x.rb
@@ -0,0 +1,21 @@
+require_relative "m2x/client"
+
+class M2X
+  VERSION = "0.0.1"
+
+  attr_reader :api_base
+  attr_reader :api_key
+
+  def initialize(api_key=nil, api_base=nil)
+    @api_base = api_base
+    @api_key  = api_key
+  end
+
+  def client
+    @client ||= ::M2X::Client.new(@api_key, @api_base)
+  end
+
+  def status
+    client.get("/status").json
+  end
+end
---
+++ lib/m2x/client.rb
@@ -0,0 +1,97 @@
+require "net/http"
+require "json"
+require "openssl"
+
+class M2X
+  class Client
+    API_BASE = "https://api-m2x.att.com/v1".freeze
+
+    VERSION = "0.0.1".freeze
+
+    CA_FILE = File.expand_path("../cacert.pem", __FILE__)
+
+    USER_AGENT = "AT&T M2X Ruby Client v#{VERSION}".freeze
+
+    def initialize(api_key=nil, api_base=nil)
+      @api_base = api_base
+      @api_key  = api_key
+    end
+
+    def request(verb, path, qs=nil, params=nil, headers=nil)
+      url = URI.parse(File.join(api_base, path))
+
+      url.query = URI.encode_www_form(qs) if qs
+
+      headers = default_headers.merge(headers || {})
+
+      body = if params
+               headers["Content-Type"] ||= "application/x-www-form-urlencoded"
+
+               case headers["Content-Type"]
+               when "application/json" then JSON.dump(params)
+               when "application/x-www-form-urlencoded" then URI.encode_www_form(params)
+               else
+                 raise ArgumentError, "Unrecognized Content-Type `#{headers["Content-Type"]}`"
+               end
+             end
+
+      options = {}
+      options.merge!(use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: CA_FILE) if url.scheme == "https"
+
+      path = url.path
+      path << "?#{url.query}" if url.query
+
+      res = Net::HTTP.start(url.host, url.port, options) do |http|
+        http.send_request(verb.to_s.upcase, path, body, headers)
+      end
+
+      Response.new(res)
+    end
+
+    [:get, :post, :put, :delete, :head, :options, :patch].each do |verb|
+      define_method verb do |path, qs=nil, params=nil, headers=nil|
+        request(verb, path, qs, params, headers)
+      end
+    end
+
+    class Response
+      attr_reader :response
+
+      def initialize(response)
+        @response = response
+      end
+
+      def raw
+        @response.body
+      end
+
+      def json
+        raise "#{@response.content_type} is not application/json" unless @response.content_type == "application/json"
+
+        @json ||= ::JSON.parse(raw)
+      end
+
+      def code
+        @code ||= @response.code.to_i
+      end
+
+      def headers
+        @headers ||= @response.to_hash
+      end
+    end
+
+    private
+
+    def api_base
+      @api_base ||= API_BASE
+    end
+
+    def default_headers
+      @headers ||= begin
+                     headers = { "User-Agent" => USER_AGENT }
+                     headers = { "X-M2X-KEY" => @api_key } if @api_key
+                     headers
+                   end
+    end
+  end
+end
