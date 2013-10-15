c9d143c08ed2fb5c426aae4bb6a345fde76bd297
Leandro López (inkel) <inkel.ar@gmail.com>
2013-10-15 09:11:00 -0300

    Add basic feeds API support

---
+++ lib/m2x/feeds.rb
@@ -0,0 +1,35 @@
+class M2X
+  class Feeds
+    def initialize(client)
+      @client = client
+    end
+
+    def list
+      @client.get("/feeds")
+    end
+
+    def view(id)
+      @client.get("/feeds/#{URI.encode(id)}")
+    end
+
+    def log(id)
+      @client.get("/feeds/#{URI.encode(id)}/log")
+    end
+
+    def location(id)
+      @client.get("/feeds/#{URI.encode(id)}/location")
+    end
+
+    def streams(id)
+      @client.get("/feeds/#{URI.encode(id)}/streams")
+    end
+
+    def stream_values(id, name)
+      @client.get("/feeds/#{URI.encode(id)}/streams/#{URI.encode(name)}/values")
+    end
+
+    def stream_put(id, name, params)
+      @client.put("/feeds/#{URI.encode(id)}/streams/#{URI.encode(name)}", {}, params)
+    end
+  end
+end
