c9dc33ad1416fbefaff0c715eee0516e1b573c9a
Leandro López (inkel) <inkel.ar@gmail.com>
2013-11-14 13:20:27 -0300

    Update limit stream values documentation

--- lib/m2x/feeds.rb
+++ lib/m2x/feeds.rb
@@ -62,7 +62,20 @@ class M2X
       @client.get("/feeds/#{URI.encode(id)}/streams/#{URI.encode(name)}")
     end
 
-    # Return a list with the latest values from a stream
+    # List values from an existing data stream associated with a
+    # specific feed, sorted in reverse chronological order (most
+    # recent values first).
+    #
+    # The values can be filtered by using one or more of the following
+    # optional parameters:
+    #
+    # * `start` An ISO 8601 timestamp specifying the start of the date
+    # * range to be considered.
+    #
+    # * `end` An ISO 8601 timestamp specifying the end of the date
+    # * range to be considered.
+    #
+    # * `limit` Maximum number of values to return.
     def stream_values(id, name, params={})
       @client.get("/feeds/#{URI.encode(id)}/streams/#{URI.encode(name)}/values", params)
     end
