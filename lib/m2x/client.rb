cdd56738bed7ec514575604706e408bd667da913
Federico Saravia <fedesaravia@gmail.com>
2014-11-20 20:56:10 -0300

    Reorganize methods for better visibility

--- lib/m2x/client.rb
+++ lib/m2x/client.rb
@@ -19,43 +19,6 @@ class M2X::Client
     @api_key  = api_key
   end
 
-  def request(verb, path, qs=nil, params=nil, headers=nil)
-    url = URI.parse(File.join(api_base, path))
-
-    url.query = URI.encode_www_form(qs) unless qs.nil? || qs.empty?
-
-    headers = default_headers.merge(headers || {})
-
-    body = if params
-             headers["Content-Type"] ||= "application/x-www-form-urlencoded"
-
-             case headers["Content-Type"]
-             when "application/json" then JSON.dump(params)
-             when "application/x-www-form-urlencoded" then URI.encode_www_form(params)
-             else
-               raise ArgumentError, "Unrecognized Content-Type `#{headers["Content-Type"]}`"
-             end
-           end
-
-    options = {}
-    options.merge!(use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: CA_FILE) if url.scheme == "https"
-
-    path = url.path
-    path << "?#{url.query}" if url.query
-
-    res = Net::HTTP.start(url.host, url.port, options) do |http|
-      http.send_request(verb.to_s.upcase, path, body, headers)
-    end
-
-    @last_response = Response.new(res)
-  end
-
-  [:get, :post, :put, :delete, :head, :options, :patch].each do |verb|
-    define_method verb do |path, qs=nil, params=nil, headers=nil|
-      request(verb, path, qs, params, headers)
-    end
-  end
-
   def status
     get("/status")
   end
@@ -98,6 +61,43 @@ class M2X::Client
 
   def keys
     M2X::Client::Key.list(self)
+  end
+
+  def request(verb, path, qs=nil, params=nil, headers=nil)
+    url = URI.parse(File.join(api_base, path))
+
+    url.query = URI.encode_www_form(qs) unless qs.nil? || qs.empty?
+
+    headers = default_headers.merge(headers || {})
+
+    body = if params
+             headers["Content-Type"] ||= "application/x-www-form-urlencoded"
+
+             case headers["Content-Type"]
+             when "application/json" then JSON.dump(params)
+             when "application/x-www-form-urlencoded" then URI.encode_www_form(params)
+             else
+               raise ArgumentError, "Unrecognized Content-Type `#{headers["Content-Type"]}`"
+             end
+           end
+
+    options = {}
+    options.merge!(use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_PEER, ca_file: CA_FILE) if url.scheme == "https"
+
+    path = url.path
+    path << "?#{url.query}" if url.query
+
+    res = Net::HTTP.start(url.host, url.port, options) do |http|
+      http.send_request(verb.to_s.upcase, path, body, headers)
+    end
+
+    @last_response = Response.new(res)
+  end
+
+  [:get, :post, :put, :delete, :head, :options, :patch].each do |verb|
+    define_method verb do |path, qs=nil, params=nil, headers=nil|
+      request(verb, path, qs, params, headers)
+    end
   end
 
   private
