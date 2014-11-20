e4bd752a110433b18960f934f2a88c7bbd647248
Federico Saravia <fedesaravia@gmail.com>
2014-11-20 17:03:24 -0300

    Refactor Device class to use M2X::Client object

--- lib/m2x/device.rb
+++ lib/m2x/device.rb
@@ -3,34 +3,20 @@
 # See https://m2x.att.com/developer/documentation/device for AT&T M2X
 # HTTP Device API documentation.
 class M2X::Client::Device
+  extend Forwardable
 
   PATH = "/devices"
 
   class << self
-    def client
-      @client ||= M2X::Client
-    end
-
-    # Return the details of the supplied device
-    def [](id)
-      res = client.get("#{PATH}/#{URI.encode(id)}")
-      if res.success?
-        json = res.json
-
-        new(json["id"], json)
-      end
-    end
-
     # List/search all the devices that belong to the user associated
     # with the M2X API key supplied when initializing M2X
     #
     # Refer to the device documentation for the full list of supported parameters
-    def list(params={})
-      res = client.get("#{PATH}", params)
+    def list(client, params={})
+      res = client.get(PATH, params)
 
-      res.json["devices"].map{ |atts| new(atts["id"], atts) } if res.success?
+      res.json["devices"].map{ |atts| new(client, atts) } if res.success?
     end
-    alias_method :search, :list
 
     # Search the catalog of public devices. This allows unauthenticated
     # users to search devices from other users that has been marked as
@@ -38,16 +24,16 @@ class M2X::Client::Device
     # its streams, view each stream metadata and its values.
     #
     # Refer to the device documentation for the full list of supported parameters
-    def catalog(params={})
+    def catalog(client, params={})
       res = client.get("#{PATH}/catalog", params)
 
-      res.json["devices"].map{ |atts| new(atts["id"], atts) } if res.success?
+      res.json["devices"].map{ |atts| new(client, atts) } if res.success?
     end
 
     # Create a new device
     #
     # Refer to the device documentation for the full list of supported parameters
-    def create(params={})
+    def create(client, params)
       res = client.post("#{PATH}", nil, params, "Content-Type" => "application/json")
       if res.success?
         json = res.json
@@ -57,32 +43,36 @@ class M2X::Client::Device
     end
   end
 
-  def client
-    self.class.client
-  end
+  attr_reader :attributes
 
-  attr_accessor :id
-  attr_accessor :attributes
+  def_delegator :@attributes, :[]
 
-  def initialize(id, attributes)
-    @id = id
+  def initialize(client, attributes)
+    @client     = client
     @attributes = attributes
   end
 
-  def base_path
-    @base_path ||= "#{PATH}/#{URI.encode(@id)}"
+  def path
+    @path ||= "#{ PATH }/#{ URI.encode(@attributes.fetch("id")) }"
+  end
+
+  # Return the details of the supplied device
+  def view
+    res = @client.get(path)
+
+    @attributes = res.json if res.success?
   end
 
   # Update an existing device details
   #
   # Refer to the device documentation for the full list of supported parameters
   def update(params={})
-    client.put(base_path, nil, params, "Content-Type" => "application/json")
+    @client.put(path, nil, params, "Content-Type" => "application/json")
   end
 
   # Return a list of access log to the supplied device
   def log
-    client.get("#{base_path}/log")
+    @client.get("#{path}/log")
   end
 
   # Return the current location of the supplied device
@@ -90,60 +80,60 @@ class M2X::Client::Device
   # Note that this method can return an empty value (response status
   # of 204) if the device has no location defined.
   def location
-    client.get("#{base_path}/location")
+    @client.get("#{path}/location")
   end
 
   # Update the current location of the device
   def update_location(params)
-    client.put("#{base_path}/location", nil, params, "Content-Type" => "application/json")
+    @client.put("#{path}/location", nil, params, "Content-Type" => "application/json")
   end
 
-  # Post stream updates
-  #
-  # This method allows posting multiple values to multiple streams
-  # belonging to a device and optionally, the device location.
-  #
-  # All the streams should be created before posting values using this method.
-  #
-  # Refer to the Device documentation for details
-  def post_updates(params)
-    client.post("#{base_path}/updates", nil, params, "Content-Type" => "application/json")
-  end
+  # # Post stream updates
+  # #
+  # # This method allows posting multiple values to multiple streams
+  # # belonging to a device and optionally, the device location.
+  # #
+  # # All the streams should be created before posting values using this method.
+  # #
+  # # Refer to the Device documentation for details
+  # def post_updates(params)
+  #   @client.post("#{path}/updates", nil, params, "Content-Type" => "application/json")
+  # end
 
-  def streams
-    ::M2X::Client::Stream.list(@id)
-  end
+  # def streams
+  #   ::M2X::Client::Stream.list(self["id"])
+  # end
 
-  def fetch_stream(name)
-    ::M2X::Client::Stream.fetch(@id, name)
-  end
+  # def fetch_stream(name)
+  #   ::M2X::Client::Stream.fetch(self["id"], name)
+  # end
 
-  def update_stream(name, params={})
-    ::M2X::Client::Stream.update(@id, name, params)
-  end
-  alias_method :create_stream, :update_stream
+  # def update_stream(name, params={})
+  #   ::M2X::Client::Stream.update(self["id"], name, params)
+  # end
+  # alias_method :create_stream, :update_stream
 
-  # Returns a list of API keys associated with the device
-  def keys(id)
-    client.get("/keys", device: @id)
-  end
+  # # Returns a list of API keys associated with the device
+  # def keys(id)
+  #   client.get("/keys", device: self["id"])
+  # end
 
-  # Creates a new API key associated to the device
-  #
-  # If a parameter named `stream` is supplied with a stream name, it
-  # will create an API key associated with that stream only.
-  def create_key(params)
-    keys_api.create(params.merge(device: @id))
-  end
+  # # Creates a new API key associated to the device
+  # #
+  # # If a parameter named `stream` is supplied with a stream name, it
+  # # will create an API key associated with that stream only.
+  # def create_key(params)
+  #   keys_api.create(params.merge(device: self["id"]))
+  # end
 
-  # Updates an API key properties
-  def update_key(key, params)
-    keys_api.update(key, params.merge(device: @id))
-  end
+  # # Updates an API key properties
+  # def update_key(key, params)
+  #   keys_api.update(key, params.merge(device: self["id"]))
+  # end
 
-  private
+  # private
 
-  def keys_api
-    @keys_api ||= ::M2X::Client::Key.new(client)
-  end
+  # def keys_api
+  #   @keys_api ||= ::M2X::Client::Key.new(@client)
+  # end
 end
