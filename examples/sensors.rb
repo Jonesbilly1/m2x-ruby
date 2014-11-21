718a66747b366d4d6809a218a40f87f550b9b49e
Federico Saravia <fedesaravia@gmail.com>
2014-11-20 22:13:43 -0300

    Add example with multiple devices

---
+++ examples/sensors.rb
@@ -0,0 +1,56 @@
+#! /usr/bin/env ruby
+
+require "time"
+require "m2x"
+
+KEY     = ENV.fetch("KEY")
+KITCHEN = ENV.fetch("KITCHEN")
+GARAGE  = ENV.fetch("GARAGE")
+
+
+client = M2X::Client.new(KEY)
+
+kitchen   = client.device(KITCHEN)
+garage    = client.device(GARAGE)
+door_open = M2X::Client::Stream.new(client, garage, "name" => "door_open")
+
+# Create the streams if they don't exist
+kitchen.create_stream("humidity")
+kitchen.create_stream("temperature")
+garage.create_stream("humidity")
+garage.create_stream("temperature")
+
+door_open.update # Create the stream if it doesn't exist
+
+@run = true
+
+stop = Proc.new { @run = false }
+
+trap(:INT,  &stop)
+trap(:TERM, &stop)
+
+door_is_open = false
+
+while @run
+  now = Time.now.iso8601
+
+  kitchen.post_updates(values: {
+                                 humidity:    [ { timestamp: now, value: rand(0..100) } ],
+                                 temperature: [ { timestamp: now, value: rand(0..24)  } ]
+                               }
+                       )
+
+  garage.post_updates(values: {
+                                humidity:    [ { timestamp: now, value: rand(0..100) } ],
+                                temperature: [ { timestamp: now, value: rand(0..24)  } ]
+                              }
+                      )
+
+  if rand > 0.75
+    door_is_open = !door_is_open
+    door_open.update_value(door_is_open ? 1 : 0)
+  end
+
+  sleep 2
+end
+
